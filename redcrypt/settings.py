cd0e1a93729ab1ea91c41a03a67dc2c55ae48c31
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2025-01-06 22:35:42 +0530

    chore: update DATABASES configuration for different environments

--- redcrypt/settings.py
+++ redcrypt/settings.py
@@ -122,12 +122,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'redcrypt.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
+
+
 # sentry_sdk.init(
 #     dsn=os.getenv('SENTRY_DSN'),
 #     integrations=[DjangoIntegration()],
